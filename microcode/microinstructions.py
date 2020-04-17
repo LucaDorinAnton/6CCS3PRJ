757fb56c63820c5ce2ca16189d1bef8f532c9753
Luca-Dorin Anton <luca-dorin.anton@kcl.ac.uk>
2020-04-17 10:20:50 +0300

    LDB microcode

--- microcode/microinstructions.py
+++ microcode/microinstructions.py
@@ -10,4 +10,8 @@ LDA.steps['other'].append(MicroStep([CtrlSigs.RO, CtrlSigs.AI], "Move RAM conten
 OUT = Instruction('OUT', 0b111111, "Move contents of reg. A to Display")
 OUT.steps['other'].append(MicroStep([CtrlSigs.AO, CtrlSigs.OUT], "Move contents of reg. A to Display"))
 
-instr_lst = [NOP, LDA, OUT]
+LDB = Instruction('LDB', 0b000010, "Load RAM Contents to Register B")
+LDB.steps['other'].append(MicroStep([CtrlSigs.IO, CtrlSigs.MI], "Move IR contetns to MAR"))
+LDB.steps['other'].append(MicroStep([CtrlSigs.RO, CtrlSigs.BI], "Move RAM contents to reg. B"))
+
+instr_lst = [NOP, LDA, LDB, OUT]
